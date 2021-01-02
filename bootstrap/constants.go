3c1f2fa7a2a67061978394c8997a44577d0f8c53
Prakhar Agnihotri <rounak316@gmail.com>
2021-01-03 03:47:05 +0530

    sync: minio Sync

--- bootstrap/constants.go
+++ bootstrap/constants.go
@@ -2,20 +2,23 @@ package bootstrap
 
 import "flag"
 
-// Container Constants
+// Configurable Constants
 var (
 	BASEPATH          = PopulateStr("BASEPATH", "/Users/meddler/Office/Workspaces/Secoflex/secoflex/modules/watchdog/tmp", "Base Path")
 	INPUTDIR          = PopulateStr("INPUTDIR", "input", "Specify output directory")
 	OUTPUTDIR         = PopulateStr("OUTPUTDIR", "ouput", "Specify output directory")
-	SCHEMADIR         = PopulateStr("SCHEMADIR", "schema", "Specify output directory")
-	LOGDIR            = PopulateStr("LOGDIR", "schema", "Specify output directory")
+	RESULTSJSON       = PopulateStr("RESULTSJSON", "ouput", "Specify output directory")
+	RESULTSSCHEMA     = PopulateStr("RESULTSSCHEMA", "schema", "Specify output directory")
+	LOGTOFILE         = PopulateBool("LOGTOFILE", false, "Specify output directory")
 	STDOUTFILE        = PopulateStr("STDOUTFILE", "schema", "Specify output directory")
 	STDERRFILE        = PopulateStr("STDERRFILE", "schema", "Specify output directory")
 	ENABLELOGGING     = PopulateBool("ENABLELOGGING", true, "Enable Logging")
 	MWXOUTPUTFILESIZE = PopulateInt("MWXOUTPUTFILESIZE", 500, "Enable Logging")
+	SAMPLEINPUTFILE   = PopulateStr("SAMPLEINPUTFILE", "PopulateStr", "Enable Logging")
+	SAMPLEOUTPUTFILE  = PopulateStr("SAMPLEOUTPUTFILE", "PopulateStr", "Enable Logging")
 )
 
-// Watchdog Constants
+// Watchdog: Constants
 var (
 	INPUTAPI      = PopulateStr("INPUTAPI", "input", "Specify output directory")
 	INPUTAPITOKEN = PopulateStr("INPUTAPITOKEN", "input", "Specify output directory")
