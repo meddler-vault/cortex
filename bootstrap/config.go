14c9b7b1c5255dba2b0024900128414da8e3bf9c
Prakhar Agnihotri <rounak316@gmail.com>
2021-01-06 23:09:12 +0530

    +Mesage Struct

--- bootstrap/config.go
+++ bootstrap/config.go
@@ -2,10 +2,11 @@ package bootstrap
 
 //MessageSpec...
 type MessageSpec struct {
-	Identifier    string            `json:"id"`
-	SystemEnviron map[string]string `json:"system_environ"`
-	Environ       map[string]string `json:"environ"`
-	Entrypoint    []string          `json:"entrypoint"`
-	Cmd           []string          `json:"cmd"`
-	Config        map[string]string `json:"config"`
+	Identifier    string            `json:"id"`             // For changing status, ingesting data, persisting FS on Storage (Bucket Name)
+	SystemEnviron map[string]string `json:"system_environ"` // SystemEnviron. Variables to inject to Watchdog & override for a particular task
+	Environ       map[string]string `json:"environ"`        // Environ. Variables to inject / override inside the actial process
+	Entrypoint    []string          `json:"entrypoint"`     // Override entrypoint
+	Cmd           []string          `json:"cmd"`            // Override CMD
+	Config        map[string]string `json:"config"`         // TODO: Config:
+	Variables     map[string]string `json:"variables"`      // TODO: Variables: Replace placeholders with actual value
 }
