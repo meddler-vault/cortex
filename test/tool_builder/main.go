19225e271033908a70294699004508f25b67e2ce
Prakhar Agnihotri <rounak316@gmail.com>
2021-02-02 18:37:33 +0530

    +works fine:

--- test/tool_builder/main.go
+++ test/tool_builder/main.go
@@ -35,7 +35,6 @@ func sendData() {
 
 		"process": {
 			"test": "variable"
-
 		}
 
 	},
@@ -46,7 +45,6 @@ func sendData() {
 		"output_dir" : "$output_dir"
 	},
 
-
 	"cmd": "/kaniko/executor",
 	"args": [  "--context=dir://$input_dir/new-bucket", "--destination=image" ,  "--tarPath=$output_dir/image.tar"  , "--no-push" ,  "--dockerfile=Dockerfile"  ],
 
@@ -54,11 +52,9 @@ func sendData() {
 
 	"environ": 
 	{
-
 		"exec_timeout": "1000" ,   
 		"TraceId":"5fde15c7ed17c3374c56990e" 
-
-	} ,
+	},
 		
 	"dependencies": [
 
@@ -78,7 +74,65 @@ func sendData() {
 			"type": "Type"
 		}
 
+	]
+  }`)
 
+}
+
+func gitCloner() {
+
+	producer.Produce(`
+  { 
+
+	"config": {
+
+		"system": {
+			"base_path": "/kaniko/fs",
+			"input_dir":  "/input",
+			"output_dir":  "/output"
+
+		} ,
+
+		"process": {
+			"test": "variable"
+		}
+
+	},
+
+	"substitute_var": true,
+	"variables": {
+		"input_dir" : "$input_dir",
+		"output_dir" : "$output_dir"
+	},
+
+	"cmd": "/kaniko/executor",
+	"args": [  "--context=dir://$input_dir/new-bucket", "--destination=image" ,  "--tarPath=$output_dir/image.tar"  , "--no-push" ,  "--dockerfile=Dockerfile"  ],
+
+	"id": "outputbucket" ,
+
+	"environ": 
+	{
+		"exec_timeout": "1000" ,   
+		"TraceId":"5fde15c7ed17c3374c56990e" 
+	},
+		
+	"dependencies": [
+
+		{
+			"id": "new-bucket",
+			"alias": "Alias dependen 1",
+			"type": "Type"
+		},
+		{
+			"id": "inputbasket_2",
+			"alias": "Alias dependen 1",
+			"type": "Type"
+		},
+		{
+			"id": "buckettest_3",
+			"alias": "Alias dependen 1",
+			"type": "Type"
+		}
 
 	]
   }`)
