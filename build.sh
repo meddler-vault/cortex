0987e930162c7065e22535c005805e08c20b4750
Your Name <you@example.com>
2024-06-21 17:55:53 +0530

    moved away from rmq  to nats

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
+# Set timezone and build the Go project
+TZ=Asia/Calcutta
 
-TZ=Asia/Calcutta 
+# Build the Go project with specific OS and architecture settings
+GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/meddler-io/watchdog/consumer.WatchdogVersion=`date +%Y.%m.%d.%H.%M.%S`" -o ./artifacts/watchdog 
 
-GOOS=linux GOARCH=amd64  go build -ldflags "-X github.com/meddler-io/watchdog/consumer.WatchdogVersion=`date +%Y.%m.%d.%H.%M.%S`"  -o ./artifacts/watchdog 
-cd ./artifacts
+# Change to the artifacts directory
+pushd ./artifacts
+
+# Execute the build script
 ./build.sh
+
+# Return to the original directory
+popd
