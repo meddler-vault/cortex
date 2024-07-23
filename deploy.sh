d429a835e645b457555041828e71250f3ad2053a
Your Name <you@example.com>
2024-07-23 17:05:22 +0530

    auto

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
-docker build --no-cache -t rounak316/watchdog:linux .
+docker build --build-arg WATCHDOG_VERSION=$(date +%Y.%m.%d.%H.%M.%S)  --no-cache -t rounak316/watchdog:linux .
 
 docker tag rounak316/watchdog:linux rounak316/watchdog:linux
 docker push rounak316/watchdog:linux
