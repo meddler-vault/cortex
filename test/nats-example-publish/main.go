27a6c759c6b8c51d613dcdbae9e658fe558edacb
Your Name <you@example.com>
2024-07-25 01:58:30 +0530

    lifecycle fix

--- test/nats-example-publish/main.go
+++ test/nats-example-publish/main.go
@@ -218,8 +218,83 @@ const sample_message_nomad_tool = `
 `
 const sample_message_nomad_topic = "6693fe232b4fdd0a70e41e32:6694310e67412eddafb7ed60"
 
+// NO-op msg
+const noop_msg = `
+{ 
+
+"config": {
+
+    "system": {
+        "base_path": "/tmp/watchdog-test",
+        "input_dir":  "/input",
+        "output_dir":  "/output",
+        "git_path": "/output/git-path",
+        "results_json":  "/outputs/results_json/results.json",
+        "git_remote": "https://github.com/studiogangster/sensibull-realtime-options-api-ingestor.git",
+        "git_mode": false,
+
+        "mount_volume": false,
+        "mount_volume_path": "minio-volume",
+        "mount_volume_s3_folder_path": "purger/nested 1/",
+        "mount_volume_s3_object_path": "./nested-2/",
+
+        "mount_volume_s3_access_key": "uaaGAF0jnXVHa7KV5eOa",
+        "mount_volume_s3_secret_key": "kiwty0-Xigruc-zyfnyj",
+        "mount_volume_s3_bucket": "minio-vapt",
+        "mount_volume_s3_host": "s3.meddler.io",
+        "mount_volume_s3_secure": true
+
+    } ,
+
+    "process": {
+        "test": "variable"
+    }
+
+},
+
+"substitute_var": true,
+"variables": {
+    "input_dir" : "$input_dir",
+    "output_dir" : "$output_dir",
+    "git_path" : "$git_path",
+    "mount_volume_s3_folder_path": "$mount_volume_s3_folder_path",
+    "mount_volume_s3_object_path": "$mount_volume_s3_object_path"
+
+
+    
+},
+
+"cmd": [
+  "echo" , "noop-performing"
+
+],
+"args": [  ],
+"entrypoint": [ 
+    "/bin/bash" ],
+
+"id": "outputbucket" ,
+
+"environ": 
+{
+    "exec_timeout": "1000" ,   
+    "TraceId":"5fde15c7ed17c3374c56990e" 
+},
+    
+"dependencies": [
+
+  
+
+]
+}
+
+`
+const noop_topic = "tasks_test"
+
 func main() {
 
-	er := producernats.Produce("whitehat", "4Jy6P)$Ep@c^SenL", "rmq.meddler.io:443", sample_message_nomad_topic, sample_message_nomad_tool)
+	er := producernats.Produce("whitehat", "4Jy6P)$Ep@c^SenL", "rmq.meddler.io:443", noop_topic, noop_msg)
+	producernats.Produce("whitehat", "4Jy6P)$Ep@c^SenL", "rmq.meddler.io:443", noop_topic, noop_msg)
+	producernats.Produce("whitehat", "4Jy6P)$Ep@c^SenL", "rmq.meddler.io:443", noop_topic, noop_msg)
+	producernats.Produce("whitehat", "4Jy6P)$Ep@c^SenL", "rmq.meddler.io:443", noop_topic, noop_msg)
 	log.Println("Error", er)
 }
