60c3a8f4037e816255065c7b94a27f8e9febb021
Your Name <you@example.com>
2024-08-03 19:00:25 +0530

    +config mounts ; result publisher

--- deploy-result-publisher.sh
+++ deploy-result-publisher.sh
@@ -1,5 +1,5 @@
 
-docker build  --progress=plain --no-cache  --build-arg WATCHDOG_VERSION=$(date +%Y.%m.%d.%H.%M.%S)  -t rounak316/watchcat:linux .
+docker build  --progress=plain --no-cache  --build-arg WATCHDOG_VERSION=$(date +%Y.%m.%d.%H.%M.%S)  -f result.Dockerfile  -t rounak316/watchcat:linux .
 
 docker tag rounak316/watchcat:linux rounak316/watchcat:linux
 docker push rounak316/watchcat:linux
